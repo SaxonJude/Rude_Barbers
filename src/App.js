2a8f2f50bd1ec28c5c6b74c5ec669f91f7f5f4cb
SaxonJude <saxonjude@gmail.com>
2023-03-10 20:26:25 +0000

    file structure setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
 // Import components
-
+import { About, CTA, Footer, Gallery, Header, Navbar, Pricing, Services } from './components';
 
 import './App.css';
 
 const App = () => {
   return (
     <div className='App'>
-        
+        <About />
+        <CTA />
+        <Footer />
+        <Gallery />
+        <Header />
+        <Navbar />
+        <Pricing />
+        <Services />
     </div>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as About } from './about/About';
+export { default as CTA } from './cta/CTA';
+export { default as Footer } from './footer/Footer';
+export { default as Gallery } from './gallery/Gallery';
+export { default as Pricing } from './pricing/Pricing';
+export { default as Services } from './services/Services.jsx';
+export { default as Navbar } from './navbar/Navbar.jsx';
+export { default as Header } from './header/Header.jsx';
