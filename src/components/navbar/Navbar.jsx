6908f5c6474a94b3fadb4f24475dc375d9cab9cd
SaxonJude <saxonjude@gmail.com>
2023-03-12 16:25:18 +0000

    added navbar styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './navbar.css';
 
+import logo from '../../assets/logo.svg';
+
 const Navbar = () => {
   return (
-    <div>
-        Navbar
+    <div className='navbar'>
+        <div className='navbar-links'>
+          <div className="navbar-links_logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="navbar-links_container">
+            <p><a href="#services">Services</a></p>
+            <p><a href="#pricing">Pricing</a></p>
+            <p><a href="#lookbook">Lookbook</a></p>
+            <p><a href="#About">About</a></p>
+          </div>
+        </div>
+        <a className='navbar-appointment' href="https://app.squarespacescheduling.com/schedule.php?owner=28691044" target='_blank'>Appointment</a>
     </div>
   )
 }
