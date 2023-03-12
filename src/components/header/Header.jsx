afe661865e47f440134aa700b48406c638edd9f8
SaxonJude <saxonjude@gmail.com>
2023-03-12 16:52:16 +0000

    added header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,8 +3,12 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='header section__padding'>
+      <div className="header-container">
+        <h1>Be Bold, Be Rude. Get the Best Haircut in Bournemouth</h1>
+        <p>From Trims to Mustaches: Rude Barbers Meets All Your Haircut Needs in One Convenient Location.</p>
+        <a className='navbar-appointment' href="https://app.squarespacescheduling.com/schedule.php?owner=28691044" target='_blank'>Appointment</a>
+      </div>
     </div>
   )
 }
