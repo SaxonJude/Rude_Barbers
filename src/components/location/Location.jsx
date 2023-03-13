1b3085c8b89d45c757311115f3ed565a7c8339df
SaxonJude <saxonjude@gmail.com>
2023-03-13 20:53:48 +0000

    added location

--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -13,21 +13,23 @@ const Location = () => {
         </div>
         <div className="location-container_box">
           <div className="location-container_box-content">
-              <div className="location_address">
-                <h3>Address</h3>
-                <p>RUDE Barbers Bournemouth, <br/> 8 Calvin Rd, <br/> Winton, <br/>Bournemouth <br/>BH9 1LN</p>
-              </div>
-              <div className="location_underline"></div>
-              <div className="location_openhours">
-                <h3>Opening Hours</h3>
-                <p>Monday - Friday</p>
-                <p>9am - 8pm</p>
-                <div className="location_text_openhours">
-                  <p>Saturday <br/>9am - 4pm</p>
-                  <p>Sunday <br/> 10am - 2pm</p>
+              <div className="location-container_box-content_inner">
+                <div className="location_address">
+                  <h3>Address</h3>
+                  <p>RUDE Barbers Bournemouth, <br/> 8 Calvin Rd, <br/> Winton, <br/>Bournemouth <br/>BH9 1LN</p>
                 </div>
+                <div className="location_underline"></div>
+                <div className="location_openhours">
+                  <h3>Opening Hours</h3>
+                  <p><b>Monday - Friday</b></p>
+                  <p>9am - 8pm</p>
+                  <div className="location_text_openhours">
+                    <p><b>Saturday</b> <br/>9am - 4pm</p>
+                    <p><b>Sunday</b> <br/> 10am - 2pm</p>
+                  </div>
+                </div>
+                <a className='navbar-appointment' href="https://app.squarespacescheduling.com/schedule.php?owner=28691044" target='_blank'>Appointment</a>
               </div>
-              <a className='navbar-appointment' href="https://app.squarespacescheduling.com/schedule.php?owner=28691044" target='_blank'>Appointment</a>
           </div>
           <div className="location-container_box-map">
               <img src={map} alt="location" />
